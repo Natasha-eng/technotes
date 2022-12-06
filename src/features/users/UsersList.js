3f378240c63b3bd11dd8946ec27fb689ccbcf811
Natasha-eng <natasha.shilova.89@gmail.com>
2022-12-06 15:18:34 +0300

    ready to deploy

---
+++ src/features/users/UsersList.js
@@ -0,0 +1,57 @@
+import React from "react";
+import User from "./User";
+import { useGetUsersQuery } from "./usersApiSlice";
+
+const UsersList = () => {
+  // const response = useGetUsersQuery();
+  // console.log("response", response);
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery("usersList", {
+    pollingInterval: 6000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgsChange: true,
+  });
+
+  let content;
+
+  if (isLoading) content = <p>Loading...</p>;
+
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = users;
+
+    const tableContent =
+      ids?.length && ids.map((userId) => <User key={userId} userId={userId} />);
+
+    content = (
+      <table className="table table--users">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th user__username">
+              Username
+            </th>
+            <th scope="col" className="table__th user__roles">
+              Roles
+            </th>
+            <th scope="col" className="table__th user__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+
+  return content;
+};
+
+export default UsersList;
